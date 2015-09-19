b2aab275c90394571e16bc2a6486b31e5ef35c4e
Sandro Wirth <s.wirth303@gmail.com>
2015-09-19 02:23:06 +0200

    Initial upload

---
+++ StockWatcher/src/com/sandrowirth/stockwatcher/client/StockPrice.java
@@ -0,0 +1,46 @@
+package com.sandrowirth.stockwatcher.client;
+
+public class StockPrice {
+	
+	private String symbol;
+	private double price;
+	private double change;
+	
+	public StockPrice() {
+	}
+
+	public StockPrice(String symbol, double price, double change) {
+		super();
+		this.symbol = symbol;
+		this.price = price;
+		this.change = change;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getChange() {
+		return change;
+	}
+	
+	public double getChangePercent() {
+		return 100.0 * this.change / this.price;
+	}
+
+	public void setChange(double change) {
+		this.change = change;
+	}
+}
